32907eb3bd05b7baa7112a5bd28a08a242eef205
HRVB <h.boulangue@gmail.com>
2019-06-26 09:53:05 +0200

    Install Twitter/Bootstrap theme - Create base.html.twig for Twig /#1

---
+++ src/Controller/PageController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PageController class file
+ *
+ * PHP Version 7.2
+ *
+ * @category Controllers
+ * @package  Controllers
+ * @author   Hervé Boulangué <h.boulangue@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://snowtricks.herveboulangue.fr/
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PageController
+ * Controller for the static pages of the site.
+ * @category Controller
+ * @package App\Controller
+ * @author   Hervé Boulangué <h.boulangue@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+class PageController extends AbstractController
+{
+    /**
+     * Matches / exactly
+     *
+     * @route("/", name="Accueil")
+     */
+    public function homePage(): Response
+    {
+        return new Response($this->renderView("index.html.twig"));
+    }
+}
